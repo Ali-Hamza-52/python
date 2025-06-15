c54de63814fb593124baa668f0d2225d13bd86bc
Ali-Hamza-52 <alihamzabrw52@gmail.com>
2025-06-15 08:25:30 +0500

    learn Arithmetic

---
+++ 01-python-Basics/02-arithmetic/main.py
@@ -0,0 +1,51 @@
+# Arithmetic operations in Python
+# Addition
+a = 10
+b = 20
+c = a + b
+print(c)
+
+# Subtraction
+a = 30
+b = 20
+c = a - b
+print(c)
+
+
+# Multiplication
+a = 10
+b = 20
+c = a * b
+print(c)
+
+
+# Division
+a = 10
+b = 20
+c = a / b
+print(c)
+
+
+# Modulus
+a = 10
+b = 20
+c = a % b
+print(c)
+
+
+# Floor division
+# Floor Division is the division that results into whole number adjusted to the left in the number line.
+# It is also known as integer division.
+a = 10
+b = 9
+c = a // b
+print(c)
+
+
+# Exponent
+# Exponentiation is the operation of raising one number to the power of another.
+# In Python, the exponentiation operator is `**`.
+a = 10
+b = 20
+c = a ** b
+print(c)
