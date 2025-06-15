d2c6ff9cc042aba057934fc9d447693bf33ee70d
Ali-Hamza-52 <alihamzabrw52@gmail.com>
2025-06-15 08:21:25 +0500

    learn Variable

---
+++ 01-python-Basics/01-variable/main.py
@@ -0,0 +1,59 @@
+# int
+a = 23
+b= 20
+print(a)
+
+# float
+c = 23.5
+d = 20.5
+print(c , d)
+
+# string
+e = "Ali"
+f = "Hamza"
+print(e , f)
+
+# boolean
+g = True
+h = False
+print(g , h)
+
+# complex
+i = 3 + 4j
+j = 5 + 6j
+print(i , j)
+
+# list
+k = [1,2,3,4,5]
+l = [6,7,8,9,10]
+print(k , l)
+
+# tuple
+m = (1,2,3,4,5)
+n = (6,7,8,9,10)
+print(m , n)
+
+# dictionary
+o = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five"}
+p = {6:"six", 7:"seven", 8:"eight", 9:"nine", 10:"ten"}
+print(o , p)
+
+# set
+q = {1,2,3,4,5}
+r = {6,7,8,9,10}
+print(q , r)
+
+# frozenset
+s = frozenset({1,2,3,4,5})
+t = frozenset({6,7,8,9,10})
+print(s , t)
+
+# byte
+u = b"Ali"
+v = b"Hamza"
+print(u , v)
+
+# None
+w = None
+x = None
+print(w , x)
